eacefcd23339d44664dd9355e5524868c8fcb8fd
YonatanOfek <antonykoafe@gmail.com>
2021-06-23 23:02:20 +0300

    noted I'll probably not use the strategy wizard. just use conditional formatting

---
+++ strategy_wizard.py
@@ -0,0 +1,6 @@
+
+# Simplest solution is to have excel conditional formatting RED for missing data as wizard! so not implementing for now.
+
+# for each item in df print "how much percentage for {Financial Instrumetn} U/A/S/?
+# 40 40 20
+
