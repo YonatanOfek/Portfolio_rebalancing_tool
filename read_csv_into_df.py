0aee425c4d81d6a3c759a4efefc85a14e25ddfe7
YonatanOfek <antonykoafe@gmail.com>
2021-06-23 23:00:46 +0300

    added csv export into pandas phase as callable funciton

--- read_csv_into_df.py
+++ read_csv_into_df.py
@@ -1,24 +1,15 @@
 import pandas as pd
 import pathlib
-import matplotlib
 
 
-filename = pathlib.Path("C:/Users/Anton/PycharmProjects/Portfolio_rebalancing_tool/portfolio_export_for_testing.csv")
-df = pd.read_csv(filename)
+def read_csv_export(export_filename: str):
+    df_ = pd.read_csv(export_filename)
+    target_columns = ['Financial Instrument', 'Size', 'Last']
+    df2 = pd.concat([df_[key] for key in target_columns], axis=1)
+    return df2
 
 
-# create new df from selected columns ( keep only instrument name, position size, last value)
-
-
-target_columns = ['Financial Instrument', 'Size', 'Last']
-df2 = pd.concat([df[key] for key in target_columns], axis=1)
-
-
-# add cash data using wizard TODO
-# USD_CASH_POSITION = 5000
-# ILS_CASH_POSITION = 49999
-
-# add strat relationship columns, and cash data using wizard TODO
-
-# strat_list = ["UD", "Sven", "Andrew", "Else"]
-
+if __name__ == '__main__':
+    filename = pathlib.Path(
+        "C:/Users/Anton/PycharmProjects/Portfolio_rebalancing_tool/portfolio_export_for_testing.csv")
+    df = read_csv_export(filename)
