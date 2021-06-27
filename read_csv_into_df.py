a5ec38708a7f1f730afe5168b4b1d2f201c6d218
YonatanOfek <antonykoafe@gmail.com>
2021-06-27 21:56:15 +0300

    - We can now take in data with alphabet characters and nan values

--- read_csv_into_df.py
+++ read_csv_into_df.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import pathlib
-import re
+import numpy as np
 
 def read_csv_export(export_filename: str):
     df_ = pd.read_csv(export_filename)
     target_columns = ['Financial Instrument', 'Position', 'Last']
     df2 = pd.concat([df_[key] for key in target_columns], axis=1)
 
-    # clean data  todo Clean position data-get rid of ',' and convert to numeric.
+    # clean data  todo Clean position data-get rid of ',' 'C' and convert to numeric.
     #             todo Clean Last data - get rid of alphabeticals and convert to numeric.
     # df2['Position'] = pd.to_numeric(df2['Position'])
     #
@@ -16,6 +16,12 @@ def read_csv_export(export_filename: str):
     #     re.sub(regex,i,'')
     #
     # df2['Last'] = pd.to_numeric(df2['Last'])
+
+
+
+    df2 = df2.replace({'Last':'[A-Za-z]'},'',regex=True)
+    df2 = df2.replace({'Last': np.nan}, '0', regex=True)
+
     return df2
 
 
