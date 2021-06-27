5e7fd888eb3cb0409696c17ce15bb2c1b1c5cc0c
YonatanOfek <antonykoafe@gmail.com>
2021-06-27 21:56:45 +0300

    - tidy up

--- read_csv_into_df.py
+++ read_csv_into_df.py
@@ -7,17 +7,6 @@ def read_csv_export(export_filename: str):
     target_columns = ['Financial Instrument', 'Position', 'Last']
     df2 = pd.concat([df_[key] for key in target_columns], axis=1)
 
-    # clean data  todo Clean position data-get rid of ',' 'C' and convert to numeric.
-    #             todo Clean Last data - get rid of alphabeticals and convert to numeric.
-    # df2['Position'] = pd.to_numeric(df2['Position'])
-    #
-    # regex = '[a-z]'
-    # for i in df2['Last']:
-    #     re.sub(regex,i,'')
-    #
-    # df2['Last'] = pd.to_numeric(df2['Last'])
-
-
 
     df2 = df2.replace({'Last':'[A-Za-z]'},'',regex=True)
     df2 = df2.replace({'Last': np.nan}, '0', regex=True)
