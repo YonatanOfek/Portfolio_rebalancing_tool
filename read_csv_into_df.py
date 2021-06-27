6d82eea369d6d1b01cd72e315df317fdc63f3891
YonatanOfek <antonykoafe@gmail.com>
2021-06-27 21:57:20 +0300

    - tidy up

--- read_csv_into_df.py
+++ read_csv_into_df.py
@@ -7,7 +7,7 @@ def read_csv_export(export_filename: str):
     target_columns = ['Financial Instrument', 'Position', 'Last']
     df2 = pd.concat([df_[key] for key in target_columns], axis=1)
 
-    df2 = df2.replace({'Last':'[A-Za-z]'},'',regex=True)
+    df2 = df2.replace({'Last':'[A-Za-z]'}, '', regex=True)
     df2 = df2.replace({'Last': np.nan}, '0', regex=True)
 
     return df2
