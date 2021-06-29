36763bdd9a22e23e8b9f2c1659b5e3ca0d29652b
YonatanOfek <antonykoafe@gmail.com>
2021-06-29 22:56:23 +0300

    deleted unnecessary files

--- classes.py
+++
@@ -1,7 +0,0 @@
-import pandas
-
-class Portfolio:
-
-class Position:
-
-class Strat:
--- strategy_wizard.py
+++
@@ -1,6 +0,0 @@
-
-# Simplest solution is to have excel conditional formatting RED for missing data as wizard! so not implementing for now.
-
-# for each item in df print "how much percentage for {Financial Instrumetn} U/A/S/?
-# 40 40 20
-
