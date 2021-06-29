10bf7edad11d3b1d9422257eceb45cb617133537
YonatanOfek <antonykoafe@gmail.com>
2021-06-29 09:27:36 +0300

    - Added data cleaning for new column
    - Added two calculated columns

--- read_csv_into_df.py
+++ read_csv_into_df.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pathlib
+import re
 import numpy as np
 
 def read_csv_export(export_filename: str):
@@ -10,13 +11,25 @@ def read_csv_export(export_filename: str):
     # Filter alphabetical chars and nan from 'Last'
     df2 = df2.replace({'Last':'[A-Za-z]'}, '', regex=True)
     df2 = df2.replace({'Last': np.nan}, '0', regex=True)
+    df2 = df2.replace({'Underlying Price': '[A-Za-z]'}, '', regex=True)
+    df2 = df2.replace({'Underlying Price': np.nan}, '0', regex=True)
 
-    # Add "Option Flag" column to df
+    # Add "Option Flag" and "Option Strike" column to df
+    option_flag = option_flag = pd.Series(np.zeros((df2.shape[0]),dtype='1>U'), name='Option_Flag')
+    option_strike = pd.Series(np.zeros((df2.shape[0]), dtype='1>U'), name='Option_Strike')
+    df2 = pd.concat([df2, option_flag, option_strike], axis=1)
 
-    # Add "Option Strike" column to df
+    mask = df2['Financial Instrument'].str.contains('[Cc][Aa][Ll][Ll]|[Pp][Uu][Tt]')
+    mask = mask.replace({True: False, False: True})  # df.where() replaces where the condition is false...
+    df2['Option_Flag'] = df2['Option_Flag'].where(mask, True)
 
-    # Add "Option - Avg px" column to df???
-
+    def func_(x):
+        string_list = re.findall('\s\d{1,4}\s|\s\d{1,4}\.\d{1,4}\s', x)
+        if len(string_list) == 1:
+            return float(string_list[0])
+        else:
+            return ''
+    df2['Option Strike'] = df2['Financial Instrument'].map(func_)
 
     return df2
 
