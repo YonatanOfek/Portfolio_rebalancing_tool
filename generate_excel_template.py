d0e8f1c31cd89c6f8179b35ad351bf9e1fd64871
YonatanOfek <antonykoafe@gmail.com>
2021-06-29 21:10:53 +0300

    - Placeholder for data passing wb/ws problem
    - Refactored cell locations and formulas into properties
    
    - Refactored script to a series of functions
    
    - Added todos for returns
    
    - Added a main()

--- generate_excel_template.py
+++ generate_excel_template.py
@@ -7,120 +7,183 @@ import numpy as np
 
 class CurrentPortfolioWorksheet(Worksheet):
 
-    def add_formatting(self):
-        # add formatting
-        pleasefillin_format = wb.add_format({'bg_color': '#FFC7CE'})
+    def __init__(self, portfolio_export_data):
+        super().__init__()
+        self.portfolio_export_data = portfolio_export_data
 
-        cond_pleasefillin_format = wb.add_format()
-        # add cash data using wizard  - - cond. formatting RED
-        ws.conditional_format('B2', {'type': 'blanks',
-                                     'format': pleasefillin_format})
-        # add strat relationship columns using wizard - cond. formatting RED.
-        ws.conditional_format(pos_list_table_range, {'type': 'blanks', 'format': pleasefillin_format})
+    @property
+    def netliq_cell_loc(self):
+        return '$B$1'
+    
+    @property
+    def usdcash_cell_loc(self):
+        return '$B$2'
+    
+    @property
+    def pos_table_name(self):
+        return 'Position_list'
+    
+    @property
+    def strat_table_name(self):
+        return 'Strat_distribution'
 
-        return
+    @property
+    def netliq_formula(self):
+        return f"{self.usdcash_cell_loc} + SUM({self.pos_table_name}[[#Data],[Netliq Contribution]])"
+
+    @property
+    def stock_market_value_formula(self):
+        return f'[[#This Row],[Position]]*[[#This Row],[Last]]'
+    
+    @property
+    def otm_formula(self):
+        return f'MAX(0,[[#This Row],[Underlying Price]]-[[#This Row],[Option Strike]])'
+    
+    @property
+    def maxing_formula(self):
+        return f'Max(0.2* [[#This Row],[Underlying Price]] - {self.otm_formula},[[#This Row],[Option Strike]] * 0.1)'
+    
+    @property
+    def short_put_market_value_formula(self):
+        return f'[[#This Row],[Position]]*(-100)*([[#This Row],[Last]]+{self.maxing_formula})'
+    
+    @property
+    def put_market_value_formula(self):
+        return 0  # todo v3
+    
+    @property
+    def short_call_market_value_formula(self):
+        return 0  # todo v3
+    
+    @property
+    def call_market_value_formula(self):
+        return 0  # todo v3
+    
+    @property
+    def polymorphic_market_value_formula(self):
+        return f'If([[#This Row],[Option Strike]] > 0,{self.short_put_market_value_formula},{self.stock_market_value_formula})'
+
+    @property
+    def short_option_netliq_contribution_formula(self):
+        return '(-100)*[[#This Row],[Last]]*[[#This Row],[Position]]'
+    
+    @property
+    def stock_netliq_contribution_formula(self):
+        return '[[#This Row],[Market Value]]'
+    
+    @property
+    def polymorphic_netliq_contribution_formula(self):
+        return f'If([[#This Row],[Option Strike]] > 0,{self.short_option_netliq_contribution_formula},{self.stock_netliq_contribution_formula})'
+
+    @property
+    def percent_netliq_formula(self):
+        return f'([[#This Row],[Market Value]])/{self.netliq_cell_loc}'
+    
+    @property
+    def weighted_exposure_formula_redhead(self):
+        return f'([[#This Row],[Market Value]])*([[#This Row],[Redhead %]])'
+    
+    @property
+    def weighted_exposure_formula_workhorse(self):
+        return f'([[#This Row], [Market Value]])*([[#This Row], [Workhorse %]])'
+    
+    @property
+    def weighted_exposure_formula_safe(self):
+        return f'([[#This Row], [Market Value]])*([[#This Row],[Safe %]])'
+
+    @property
+    def redhead_strat_summing_formula(self):
+        return f'SUM({self.pos_table_name}[[#Data],[Redhead]])'
+    @property
+    def workhorse_strat_summing_formula(self):
+        return f'SUM({self.pos_table_name}[[#Data],[Workhorse]])'
+    @property
+    def safe_strat_summing_formula(self):
+        return f'SUM({self.pos_table_name}[[#Data],[Safe]])'
+
+    @property
+    def topleft_corner_of_t1(self):
+        return [2, 1]  # B3 todo add xl to row here too..
+
+    @property
+    def topleft_corner_of_t2(self):
+        return [2, self.portfolio_export_data.shape[1] + 10] # todo add xl to row here too..
+
+    @property
+    def t1_range(self):
+        return [self.topleft_corner_of_t1[0], self.topleft_corner_of_t1[1],
+                self.portfolio_export_data.shape[0] + self.topleft_corner_of_t1[0], self.portfolio_export_data.shape[1] + 8]
+
+    @property
+    def t2_range(self):
+        return [self.topleft_corner_of_t2[0], self.topleft_corner_of_t2[1], self.topleft_corner_of_t2[0] + 3,
+                self.topleft_corner_of_t2[1] + 1]
 
     def add_warnings_and_misc_cells(self):
-        netliq_cell_loc = '$B$1'
-        usdcash_cell_loc = '$B$2'
-        # Add notes and netliq, cash
 
-        ws.write('A1', 'Net Liquidity:')
+        # Add netliq, cash
+        self.write('A1', 'Net Liquidity:') # todo incorporate xl_row_col_to_str methods to make this proper...
+        self.write_formula(self.netliq_cell_loc, f'={self.netliq_formula}')
 
-        ws.write_formula(netliq_cell_loc,
-                         f'={usdcash_cell_loc} + SUM({t1_name}[[#Data],[Netliq Contribution]])')  # todo this is dumb because doesn't consider premia given from put options rather margin load...
+        self.write('A2', 'USD Cash Position:')
 
-        ws.write('A2', 'USD Cash Position:')
-        ws.write('B2', '')
-        ws.set_column(data.shape[1] + 15, data.shape[1] + 15, width=115)
-        ws.write(0, data.shape[1] + 15,
-                 'Note for OPTION POSITIONS - Margin requirement can change at any time, so position sizing values SHOULD BE REVIEWED MANUALLY',
-                 wb.add_format({'bold': True, 'bg_color': 'black', 'font_color': 'red'}))
+        # Add warning
+        self.set_column('V:V', width=115)  #data.shape[1] + 15, data.shape[1] + 15 todo incorporate xl_row_col_to_str methods to make this proper...
+        self.write('V1',                     # 0, data.shape[1] + 15, todo incorporate xl_row_col_to_str methods to make this proper...
+                   'Note for OPTION POSITIONS - Margin requirement can change at any time, so position sizing values SHOULD BE REVIEWED MANUALLY')
 
         return
 
-    def add_positions_list_table(self, portfolio_export_data):
-        # initate a table for one position and generate all the formulas
+    def add_positions_list_table(self):
 
-        t1_name = 'Position_list'
-
-        stock_market_value_component = f'[[#This Row],[Position]]*[[#This Row],[Last]]'
-        # do something for puts and short assets
-        # Put Price + Maximum ((20% 2* Underlying Price - Out of the Money Amount),(10% * Strike Price))
-        otm_component = f'MAX(0,[[#This Row],[Underlying Price]]-[[#This Row],[Option Strike]])'
-        maxing_component = f'Max(0.2* [[#This Row],[Underlying Price]] - {otm_component},[[#This Row],[Option Strike]] * 0.1)'
-        short_put_market_value_component = f'[[#This Row],[Position]]*(-100)*([[#This Row],[Last]]+{maxing_component})'
-        put_market_value_formula = 0  # todo v3
-        short_call_market_value_formula = 0  # todo v3
-        call_market_value_formula = 0  # todo v3
-        polymorphic_market_value_formula = f'=If([[#This Row],[Option Strike]] > 0,{short_put_market_value_component},{stock_market_value_component})'
-
-        short_option_netliq_contribution_component = '(-100)*[[#This Row],[Last]]*[[#This Row],[Position]]'
-        stock_netliq_contribution_component = '[[#This Row],[Market Value]]'
-        polymorphic_netliq_contribution_formula = f'=If([[#This Row],[Option Strike]] > 0,{short_option_netliq_contribution_component},{stock_netliq_contribution_component})'
-
-        percent_netliq_formula = f'=([[#This Row],[Market Value]])/{netliq_cell_loc}'
-        weighted_exposure_formula_redhead = f'=([[#This Row],[Market Value]])*([[#This Row],[Redhead %]])'
-        weighted_exposure_formula_workhorse = f'=([[#This Row], [Market Value]])*([[#This Row], [Workhorse %]])'
-        weighted_exposure_formula_safe = f'=([[#This Row], [Market Value]])*([[#This Row],[Safe %]])'
-
-        # add table
-        topleft_corner_of_t1 = [2, 1]  # B1
-        t1_range = [topleft_corner_of_t1[0], topleft_corner_of_t1[1], data.shape[0] + topleft_corner_of_t1[0],
-                    data.shape[1] + 8]
-
-        ws.add_table(t1_range[0], t1_range[1], t1_range[2], t1_range[3], {'name': f'{t1_name}',
-                                                                          'data': data,
+        self.add_table(self.t1_range[0], self.t1_range[1], self.t1_range[2], self.t1_range[3], {'name': f'{self.pos_table_name}',
+                                                                          'data': self.portfolio_export_data,
                                                                           'columns': [
-                                                                              {'header': 'Financial Instrument'},
+                                                                              {'header': 'Financial Instrument'}, # todo get these programmatically after switching data from a df.values into a df
                                                                               {'header': 'Position'},
                                                                               {'header': 'Last'},
                                                                               {'header': 'Underlying Price'},
                                                                               {'header': 'Option Strike'},
                                                                               {'header': 'Market Value',
-                                                                               'formula': polymorphic_market_value_formula},
+                                                                               'formula': f'={self.polymorphic_market_value_formula}'},
                                                                               {'header': 'Netliq Contribution',
-                                                                               'formula': polymorphic_netliq_contribution_formula},
+                                                                               'formula': f'={self.polymorphic_netliq_contribution_formula}'},
                                                                               {'header': '% of Net Liq',
-                                                                               'formula': percent_netliq_formula},
+                                                                               'formula': f'={self.percent_netliq_formula}'},
                                                                               {'header': 'Redhead %',
-                                                                               'format': cond_pleasefillin_format},
+                                                                               'format': f'={self.cond_pleasefillin_format}'}, # todo wb/ws problem
                                                                               {'header': 'Workhorse %',
-                                                                               'format': cond_pleasefillin_format},
+                                                                               'format': f'={self.cond_pleasefillin_format}'},
                                                                               {'header': 'Safe %',
-                                                                               'format': cond_pleasefillin_format},
+                                                                               'format': f'={self.cond_pleasefillin_format}'},
                                                                               {'header': 'Redhead',
-                                                                               'formula': weighted_exposure_formula_redhead},
+                                                                               'formula': f'={self.weighted_exposure_formula_redhead}'}, # todo add some polymorphism
                                                                               {'header': 'Workhorse',
-                                                                               'formula': weighted_exposure_formula_workhorse},
+                                                                               'formula': f'={self.weighted_exposure_formula_workhorse}'},
                                                                               {'header': 'Safe',
-                                                                               'formula': weighted_exposure_formula_safe}
+                                                                               'formula': f'={self.weighted_exposure_formula_safe}'}
                                                                               ]})
 
-        pos_list_table_range = ws.tables[0]['range']
+        pos_list_table_range = self.tables[0]['range'] # todo do I need this considering pos_table_range
+        # add strat relationship columns using wizard - cond. formatting RED.
+        self.curr_port_ws.conditional_format(pos_list_table_range, {'type': 'blanks', 'format': self.input_is_required_format}) # todo
 
-        return
+
+        return # todo returns
 
     def add_strategies_table(self, strategy_names):
-        # initialize t2
-        t2_name = 'Strat_distribution'
-        topleft_corner_of_t2 = [2, data.shape[1] + 10]
-        t1_range = [topleft_corner_of_t2[0], topleft_corner_of_t2[1], topleft_corner_of_t2[0] + 3,
-                    topleft_corner_of_t2[1] + 1]
-        data2 = np.zeros([3, 2], '<U1')
+        # data2 = np.zeros([3, 2], '<U1') todo not need this right?
 
-        # calculate strat distribution in table
-
-        ws.add_table(t1_range[0], t1_range[1], t1_range[2], t1_range[3], {'name': t2_name,
-                                                                          'data': data2,
-                                                                          'columns': [{'header': 'Strategy'},
-                                                                                      {'header': 'Portfolio Weight'}
-                                                                                      ]})
+        self.add_table(self.t1_range[0], self.t1_range[1], self.t1_range[2], self.t1_range[3], {'name': self.strat_table_name,
+                                                                            # 'data': data2 todo not need this right?
+                                                                            'columns': [{'header': 'Strategy'},
+                                                                                        {'header': 'Portfolio Weight'}
+                                                                                        ]})
         for i, j in zip([1, 2, 3], ['Redhead', 'Workhorse', 'Safe']):
-            ws.write(topleft_corner_of_t2[0] + i, topleft_corner_of_t2[1], j)
-        for i, j in zip([1, 2, 3], [f'=SUM({t1_name}[[#Data],[Redhead]])', f'=SUM({t1_name}[[#Data],[Workhorse]])',
-                                    f'=SUM({t1_name}[[#Data],[Safe]])']):
-            ws.write_formula(topleft_corner_of_t2[0] + i, topleft_corner_of_t2[1] + 1, j)
+            self.write(self.topleft_corner_of_t2[0] + i, self.topleft_corner_of_t2[1], j)
+        for i, j in zip([1, 2, 3], [f'={self.redhead_strat_summing_formula}', f'={self.workhorse_strat_summing_formula}',
+                                    f'={self.safe_strat_summing_formula}']):
+            self.write_formula(self.topleft_corner_of_t2[0] + i, self.topleft_corner_of_t2[1] + 1, j)
 
         return
 
@@ -129,20 +192,50 @@ class CurrentPortfolioWorksheet(Worksheet):
         strategy_destribution_chart = wb.add_chart({'type': 'pie'})
         strategy_destribution_chart.set_title({'name': 'Portfolio Strategies Distribution Chart'})
         strategy_destribution_chart.add_series({
-            'categories': f'={t2_name}[Strategy]',
-            'values': f'={t2_name}[Portfolio Weight]',
+            'categories': f'={self.strat_table_name}[Strategy]',
+            'values': f'={self.strat_table_name}[Portfolio Weight]',
             'data_labels': {'percentage': True},
         })
-        ws.insert_chart(topleft_corner_of_t2[0], topleft_corner_of_t2[1] + 5, strategy_destribution_chart)
+        self.insert_chart(self.topleft_corner_of_t2[0], self.topleft_corner_of_t2[1] + 5, strategy_destribution_chart)
 
-        return
+        return # todo returns
+
+
+class PortfolioBalanceWorkbook(xlsxwriter.Workbook):
+    """
+    This object creates the Portfolio Balance Workbook. It calls methods in the CurrentPortfolioWorkSheet object which it
+    generates during its run.
+    """
+
+    def __init__(self, portfolio_export_data, filename=None, options=None):
+        super().__init__(filename, options)
+        self.curr_port_ws = self.add_worksheet('Current Portfolio', worksheet_class=CurrentPortfolioWorksheet, data=portfolio_export_data)
+
+    @property
+    def input_is_required_format(self):
+        return self.add_format({'bg_color': '#FFC7CE'})
+
+    def add_formatting(self):
+        self.curr_port_ws.conditional_format(self.curr_port_ws.usdcash_cell_loc, {'type': 'blanks',
+                                                                                  'format': self.input_is_required_format})
+        self.curr_port_ws.write('V1', wb.add_format({'bold': True, 'bg_color': 'black', 'font_color': 'red'}))
+
+    def create_curr_port_worksh(self):
+        self.curr_port_ws.add_warnings_and_misc_cells()
+        self.curr_port_ws.add_positions_list_table()
+        self.curr_port_ws.add_strategies_table()
+        self.curr_port_ws.add_strat_dist_chart()
+        self.add_formatting()
+
+        return # todo returns
 
 if __name__ == '__main__':
-        filename_ = 'C:/Users/Anton/PycharmProjects/Portfolio_rebalancing_tool/outputs/Portfolio_mgmt_testing.xlsx'
-        wb = xlsxwriter.Workbook(filename_, {'nan_inf_to_errors': True})
-        data = read_csv_export(pathlib.Path("C:/Users/Anton/PycharmProjects/Portfolio_rebalancing_tool/input_files_for_scripts/portfolio_export_for_testing_v2.csv")).values  # todo why n = 59?
-        ws = wb.add_worksheet('Current Portfolio', worksheet_class=CurrentPortfolioWorksheet)
-        ws.add_positions_list_table(data)
-        wb.close()
+    data_filename = pathlib.Path(
+        "C:/Users/Anton/PycharmProjects/Portfolio_rebalancing_tool/input_files_for_scripts/portfolio_export_for_testing_v2.csv")
+    data_ = read_csv_export(data_filename).values
+    workbook_filename = 'C:/Users/Anton/PycharmProjects/Portfolio_rebalancing_tool/outputs/Portfolio_mgmt_testing.xlsx'
+    wb = PortfolioBalanceWorkbook(workbook_filename, {'nan_inf_to_errors': True})
+    wb.create_curr_port_worksh(data_)
+    wb.close()
 
 
